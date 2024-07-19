121dda9543cea4148e49b872076f6f3e32062e40
Sash107 <suyashupadhyay2004@gmail.com>
2024-07-19 11:23:22 +0530

    basic homepage

---
+++ android/Suyash_Upadhyay/Homepage/myapp/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/homepage/homepage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "HomeApp",
+    home: myhomepage(),
+  ));
+}
