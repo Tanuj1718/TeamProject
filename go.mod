aa905b3a134cdba96d0761b7f1854295eedee142
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-07-23 13:42:09 +0530

    Enabled CORS

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.4
 require (
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
+	github.com/rs/cors v1.11.0
 	go.mongodb.org/mongo-driver v1.16.0
 	golang.org/x/crypto v0.25.0
 )
--- golang/main.go
+++ golang/main.go
@@ -5,12 +5,22 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/rs/cors"
 	"main.go/golang/router"
 )
 
 func main() {
 	r := router.Router()
-
-	fmt.Println("Server is getting ready...")
-	log.Fatal(http.ListenAndServe(":8000", r))
+		// Setup CORS middleware
+		corsOptions := cors.New(cors.Options{
+			AllowedOrigins:   []string{"*"}, 
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Content-Type", "Authorization"},
+			AllowCredentials: true,
+		})
+	
+		// Apply the CORS middleware to your router
+		router := corsOptions.Handler(r)
+		fmt.Println("Server is getting ready...")
+		log.Fatal(http.ListenAndServe(":8000", router))
 }
